2fbf8b13badd4c60b8528f3098d1149a9adc015c
Bogdan Ripa <bogdan.ripa@vectorwatch.com>
2016-05-24 16:17:07 +0300

    Hello World stream sample

---
+++ streams/hello-world.js
@@ -0,0 +1,29 @@
+// trigger the debugger so that you can easily set breakpoints
+debugger;
+
+var VectorWatch = require('vectorwatch-browser');
+
+var vectorWatch = new VectorWatch({
+    streamUID: process.env.STREAM_UID,
+    token: process.env.VECTOR_TOKEN
+});
+
+vectorWatch.on('config', function(event, response) {
+    // your stream was just dragged onto a watch face
+    console.log('on config');
+
+    response.send();
+});
+
+vectorWatch.on('subscribe', function(event, response) {
+    // your stream was added to a watch face
+    console.log('on subscribe');
+
+    response.setValue("Hello World!");
+    response.send();
+});
+
+vectorWatch.on('unsubscribe', function(event, response) {
+    // your stream was removed from a watch face
+    console.log('on unsubscribe');
+});
